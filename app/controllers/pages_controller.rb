273d8a21a51b468ab82a00da408a78fd99cbf3d6
thecookieorg <marko.manojlovic.bg@gmail.com>
2020-12-30 14:22:32 -0800

    added posts, user model with views and added some basic styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def stimulus_reflex
+  end
+
+  def hotwire
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users
+
+  get '/stimulus_reflex' => 'pages#stimulus_reflex'
+  get '/hotwire' => 'pages#hotwire'
+
   root 'pages#index'
 end
